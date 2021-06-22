beb59541d6f607d31f8af415ed05028017696e5c
Viveks295 <viveksaravanan295@gmail.com>
2021-06-22 17:39:02 -0500

    added controllers

--- src/main/java/com/springboot/webdata/controller/HomeController.java
+++ src/main/java/com/springboot/webdata/controller/HomeController.java
@@ -1,18 +1,24 @@
 package com.springboot.webdata.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 public class HomeController {
-    @RequestMapping("home")
-    public String home(){
-        System.out.println("hi");
-        return "home.html";
+    //@RequestMapping("home")
+    public String home(ModelMap model, Principal user){
+        //System.out.println("hi");
+        String userName = user.getName();
+        model.addAttribute("myHeading", userName + "'s Page");
+        return "home";
     }
     @RequestMapping("about")
     public String about() {
         System.out.println("about");
         return "about.html";
     }
+
 }
